38f42de1b1b6dde05bacacc5fa756af6d54ad452
kutlu <kutlugungor58@hotmail.com>
2019-04-16 14:12:31 +0300

    Add Login Test

--- SeleniumTestProject/UnitTest1.cs
+++ SeleniumTestProject/UnitTest1.cs
@@ -1,8 +1,10 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
 using System;
 using System.IO;
+using System.Threading;
 
 namespace Tests
 {
@@ -25,6 +27,7 @@ namespace Tests
         [TearDown]
         public void Close()
         {
+            Thread.Sleep(3000);
             driver.Quit();
         }
 
@@ -34,6 +37,19 @@ namespace Tests
             var title = driver.Title;
 
             Assert.IsTrue(title == "n11.com - Alışverişin Uğurlu Adresi");
+        }
+
+        [Test]
+        public void LoginTest()
+        {
+            driver.Navigate().GoToUrl("http://www.n11.com/giris-yap");
+            Assert.IsTrue(driver.Title == "Giriş Yap - n11.com");
+            driver.FindElementById("email").SendKeys("kutlugungor58@hotmail.com");
+            driver.FindElementById("password").SendKeys("k.456289");
+            driver.FindElementById("loginButton").Click();
+
+            var text = driver.FindElementByCssSelector(".menuLink.user").Text;
+            Assert.IsTrue(text == "Sümeyye Keskin");
         }
     }
 }
